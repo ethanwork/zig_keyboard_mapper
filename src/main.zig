e89b0745ca96cb631e66433d9b881affa017948c
Ethan <ethanw.work@outlook.com>
2025-02-07 00:42:27 -0800

    more updates

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,19 @@ const std = @import("std");
 
 // Windows API function signatures
 extern "user32" fn SetWindowsHookExW(idHook: c_int, lpfn: *const fn (c_int, usize, ?*KBDLLHOOKSTRUCT) callconv(.C) c_int, hMod: ?*usize, dwThreadId: u32) ?*usize;
+extern "user32" fn UnhookWindowsHookEx(hhk: ?*usize) callconv(.C) bool;
 extern "user32" fn CallNextHookEx(hhk: ?*usize, nCode: c_int, wParam: usize, lParam: ?*KBDLLHOOKSTRUCT) callconv(.C) c_int;
 extern "user32" fn GetMessageW(lpMsg: *MSG, hWnd: ?*usize, wMsgFilterMin: u32, wMsgFilterMax: u32) c_int;
 extern "user32" fn TranslateMessage(lpMsg: *const MSG) c_int;
 extern "user32" fn DispatchMessageW(lpMsg: *const MSG) c_int;
+extern "user32" fn GetKeyboardState(lpKeyState: [*]u8) callconv(.C) bool;
+
+const win = struct {
+    const WM_KEYDOWN = 0x0100;
+    const WM_KEYUP = 0x0101;
+    const WM_SYSKEYDOWN = 0x0104;
+    const WM_SYSKEYUP = 0x0105;
+};
 
 // Windows Structs
 const MSG = extern struct {
@@ -36,7 +45,17 @@ var hook_handle: ?*usize = null;
 // Define keyboard hook callback as a function pointer
 fn keyboardHookCallback(nCode: c_int, wParam: usize, lParam: ?*KBDLLHOOKSTRUCT) callconv(.C) c_int {
     if (nCode >= 0 and lParam != null) {
-        std.debug.print("Key Pressed: {}\n", .{lParam.?.vkCode});
+        var eventType: []const u8 = "Unknown event";
+
+        switch (wParam) {
+            win.WM_KEYDOWN => eventType = "WM_KEYDOWN",
+            win.WM_KEYUP => eventType = "WM_KEYUP",
+            win.WM_SYSKEYDOWN => eventType = "WM_SYSKEYDOWN",
+            win.WM_SYSKEYUP => eventType = "WM_SYSKEYUP",
+            else => {},
+        }
+
+        std.debug.print("Key Event: {s}, vkCode: {}\n", .{ eventType, lParam.?.vkCode });
     }
     return CallNextHookEx(hook_handle, nCode, wParam, lParam);
 }
@@ -50,6 +69,8 @@ pub fn main() !void {
     const hInstance: ?*usize = null; // No module handle needed for global hooks
 
     hook_handle = SetWindowsHookExW(WH_KEYBOARD_LL, keyboardHookPtr, hInstance, 0);
+    defer _ = UnhookWindowsHookEx(hook_handle);
+
     if (hook_handle == null) {
         std.debug.print("Failed to set keyboard hook\n", .{});
         return;
